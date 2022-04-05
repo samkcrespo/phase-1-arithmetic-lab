97c841b266ebf9779c3050b4ec902ebbe7ddd898
Samantha Crespo <samkcrespo@gmail.com>
2022-04-04 17:35:46 -0700

    commit tree f012d58d80bd36e34fcd65ad5d1f2d1a503f16b1

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
 
 // Write your code here
+
+const random = Math.floor(Math.random() * 0) + 1;
+
+
+let num3 = 44;
+let num4 = 10;
+
+const mod = num3 % num4;
+
+const max = Math.max(10,20);
